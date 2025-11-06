0005126786a361b91d75003a27e74f9373cc7fbd
shmuelharush <shmuelharush9@gmail.com>
2025-11-06 20:35:44 +0200

    completed assignment

--- Demo.java
+++ Demo.java
@@ -1,12 +1,8 @@
-// Prints the numbers 0 to 5.
-// (Same program as shown in lecture 1-1).
+
 public class Demo {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
+
         int i = 0;
-        int x = 5; 
-        int y = 9; 
-        
-        System.err.println( x + y);// Complete the program's code here:
+       
     }
  }
