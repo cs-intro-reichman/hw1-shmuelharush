24eef24931566d98c31f29d2a8bf08047e3a5edf
shmuelharush <shmuelharush9@gmail.com>
2025-11-06 19:57:32 +0200

    completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -6,7 +6,7 @@ public class FVCalc {
 		double rate = (1 + rate1/100);
 		int years = Integer.parseInt(args[2]);
 		double expValue = (currentValue * Math.pow(rate, years));
-		System.out.println("After " + years + " years, " + currentValue + "$ saved at " + rate1 + "% will yield " + (int)expValue + "$");
+		System.out.println("After " + years + " years, " + currentValue"$" + " saved at " + rate1 + " will yield% " + (int)expValue"$");
 	}
 }
 
