85cf660ad820846bd80ceb22eb975597ca68f13c
shmuelharush <shmuelharush9@gmail.com>
2025-11-06 19:37:36 +0200

    completed assignment

--- Bill3.java
+++ Bill3.java
@@ -9,7 +9,7 @@ public class Bill3
 		String name3 = args[2];
 		int total = Integer.parseInt(args[3]);
 		double split = Math.ceil(total / 3.0);
-		System.out.println("dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + split + " shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + split + " Shekels each,");
 		
 	}
 }
