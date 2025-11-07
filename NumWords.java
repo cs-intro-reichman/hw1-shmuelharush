2b519e6cf529ac1e1632e95f6cfa68f7c57343ac
shmuelharush <shmuelharush9@gmail.com>
2025-11-07 08:12:05 +0200

    completed assignmen

--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,7 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int input = Integer.parseInt(args[0]); 
-		int hundreds = (input/100)%10;
+		int hundreds = (input/100);
 		int tens = (input/10)%10;
 		int ones = (input)%10;
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones. " );
